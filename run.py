b3183830baf8a1d540702119ba45a73b3d24a83b
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-02-09 00:34:22 +0000

    following lessons

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 from taskmanager import app
-
+from taskmanager import db
 
 if __name__ == "__main__":
     app.run(
@@ -8,3 +8,10 @@ if __name__ == "__main__":
         port=int(os.environ.get("PORT")),
         debug=os.environ.get("DEBUG")
     )
+
+
+def create_tables(self):
+    with app.app_context():
+        db.create_all()
+
+create_tables(db)
