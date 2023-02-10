67a3970a0b72d2c88d8e51563d6b048c9248def7
alexkisielewicz <aleksander.kisielewicz@gmail.com>
2023-02-10 16:14:42 +0000

    Implement add task funcitonality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,11 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(elems, options);
-  });
+    M.Sidenav.init(sidenav);
 
-  // Or with jQuery
+    // datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmm, yyyy",
+      i18n: {done: "Select"}
+    });
 
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
+    // select dropdown initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
   });
